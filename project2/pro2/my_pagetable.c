1af271bd6da63c75c2feeed86ebd51313d1c94a2
Polynomia <wyrsjtu@163.com>
2017-06-01 23:17:03 +0800

    syscall

---
+++ project2/pro2/my_pagetable.c
@@ -0,0 +1,62 @@
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/unistd.h>
+#include <linux/mman.h>
+#include <linux/mm.h>
+#include <asm/pgtable.h>
+#include <linux/list.h>
+#include <asm/memory.h>
+#include <linux/slab.h> //alloction function
+#include <linux/uaccess.h>	//KtoU
+#include <linux/syscalls.h>
+#include <asm/errno.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+static int(*oldcall)(void);
+//static int(*oldcall2)(void);
+#define __NR_layoutcall 356
+//#define __NR_exposecall 378
+
+struct pagetable_layout_info {
+    uint32_t pgdir_shift;
+    uint32_t page_shift;
+};
+
+int layoutcall(struct pagetable_layout_info * pgtbl_info)
+{
+    struct pagetable_layout_info layout_info;
+    layout_info.pgdir_shift = PGDIR_SHIFT;
+    layout_info.page_shift = PAGE_SHIFT;
+    if (copy_to_user(pgtbl_info, &layout_info,sizeof(struct pagetable_layout_info)))
+        return -EFAULT;
+    return 0;
+}
+
+//No need to change them
+
+static int addsyscall_init(void)
+{
+    long *syscall = (long*)0xc000d8c4;
+    oldcall = (int(*)(void))(syscall[__NR_layoutcall]);
+ //   oldcall2=(int(*)(void))(syscall[__NR_exposecall]);
+    syscall[__NR_layoutcall] = (unsigned long )layoutcall;//only need to change the name.
+//    syscall[__NR_exposecall] = (unsigned long )exposecall;
+    printk(KERN_INFO "module load!\n");
+    return 0;
+}
+
+
+
+static void addsyscall_exit(void)
+{
+    long *syscall=(long*)0xc000d8c4;
+    syscall[__NR_layoutcall] = (unsigned long )oldcall;
+//    syscall[__NR_exposecall]=(unsigned long)oldcall2;
+    printk(KERN_INFO "module exit!\n");
+}
+
+module_init(addsyscall_init);
+module_exit(addsyscall_exit);
